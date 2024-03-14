f0531060c20aaa97457ac1b8129b9b5a0f0ab5a6
Vasily Zinovyev <zinavasa@gmail.com>
2024-03-14 11:44:18 +0300

    flake3

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-
 from api import views
-
-b = 'poop'
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
